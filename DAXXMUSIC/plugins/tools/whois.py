7d7e2c4ab678ffee1367e26f20244bfcc3323896
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-14 23:28:15 +0530

    Update whois.py

--- DAXXMUSIC/plugins/tools/whois.py
+++ DAXXMUSIC/plugins/tools/whois.py
@@ -20,12 +20,13 @@ def ReplyCheck(message: Message):
 
 
 infotext = (
-    "❀ [{full_name}](tg://user?id={user_id}) ❀\n\n"
-    " ๏ ᴜsᴇʀ ɪᴅ ➠ `{user_id}`\n"
-    " ๏ ғɪʀsᴛ ɴᴀᴍᴇ ➠ `{first_name}`\n"
-    " ๏ ʟᴀsᴛ ɴᴀᴍᴇ ➠ `{last_name}`\n"
-    " ๏ ᴜsᴇʀɴᴀᴍᴇ ➠ `@{username}`\n"
-    " ๏ ʟᴀsᴛ sᴇᴇɴ ➠ `{last_online}`"
+    " ❖ ᴜsᴇʀ sʜᴏʀᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ⏤͟͟͞͞★\n\n"
+    " ● ᴜsᴇʀ ɪᴅ ➥ `{user_id}`\n"
+    " ● ғɪʀsᴛ ɴᴀᴍᴇ ➥ `{first_name}`\n"
+    " ● ʟᴀsᴛ ɴᴀᴍᴇ ➥ `{last_name}`\n"
+    " ● ᴜsᴇʀɴᴀᴍᴇ ➥ `@{username}`\n"
+    " ● ʟᴀsᴛ sᴇᴇɴ ➥ `{last_online}`\n\n"
+    ” ❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐”
 )
 
 
