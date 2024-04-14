c4989d3a4b01a57cdde2e6d9ac31d36d656755e2
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-15 00:09:03 +0530

    Update telegraph.py

--- DAXXMUSIC/plugins/tools/telegraph.py
+++ DAXXMUSIC/plugins/tools/telegraph.py
@@ -2,19 +2,25 @@ from telegraph import upload_file
 from pyrogram import filters
 from DAXXMUSIC import app
 from pyrogram.types import InputMediaPhoto
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+EVAA = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+    ],
+]
 
 @app.on_message(filters.command(["tgm" , "tm"]))
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
-        i = message.reply("๏ ᴍᴀᴋᴇ ᴀ ʟɪɴᴋ...")
+        i = message.reply("❖ ᴍᴀᴋᴇ ᴀ ʟɪɴᴋ...")
         path = reply.download()
         fk = upload_file(path)
         for x in fk:
             url = "https://telegra.ph" + x
 
-        i.edit(f'๏ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴘʜ ᴜʀʟ ɪs ʀᴇᴀᴅʏ ʙᴀʙʏ ➠ {url}\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐')
+        i.edit(f'❖ ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴘʜ ᴜʀʟ ɪs ʀᴇᴀᴅʏ ʙᴀʙʏ ➥ `{url}`\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ɴ ʏ ᴋ ᴀ ᴀ ࿐' , reply_markup=InlineKeyboardMarkup(EVAA),)
 
 ########____________________________________________________________######
 
@@ -22,11 +28,11 @@ def ul(_, message):
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
-        i = message.reply("๏ ᴍᴀᴋᴇ ᴀ ʟɪɴᴋ...")
+        i = message.reply("❖ ᴍᴀᴋᴇ ᴀ ʟɪɴᴋ...")
         path = reply.download()
         fk = upload_file(path)
         for x in fk:
             url = "https://graph.org" + x
 
-        i.edit(f'๏ ʏᴏᴜʀ ɢʀᴀᴘʜ ᴜʀʟ ɪs ʀᴇᴀᴅʏ ʙᴀʙʏ ➠ {url}\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐')
+        i.edit(f'❖ ʏᴏᴜʀ ɢʀᴀᴘʜ ᴜʀʟ ɪs ʀᴇᴀᴅʏ ʙᴀʙʏ ➥ `{url}`\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ɴ ʏ ᴋ ᴀ ᴀ ࿐' , reply_markup=InlineKeyboardMarkup(EVAA),)
 
