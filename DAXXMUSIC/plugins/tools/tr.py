695dbef6b2940e9f5899a1f367a96ffcd9049527
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-15 02:30:00 +0530

    Update tr.py

--- DAXXMUSIC/plugins/tools/tr.py
+++ DAXXMUSIC/plugins/tools/tr.py
@@ -33,6 +33,6 @@ async def translate(_, message) -> None:
     translation = await trans(to_translate, sourcelang=source, targetlang=dest)
     reply = (
         f"✦ ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ {source} ᴛᴏ {dest} ➥\n\n"
-        f"```♥︎ {translation.text}```"
+        f"```♥︎ {translation.text}```\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ๛ɴ ʏ ᴋ ᴀ ᴀ ࿐"
     )
     await message.reply_text(reply)
