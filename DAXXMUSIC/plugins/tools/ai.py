2c5dae3ab2b6a3446e1cb18cf7ec1bb364273609
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-15 02:27:06 +0530

    Update ai.py

--- DAXXMUSIC/plugins/tools/ai.py
+++ DAXXMUSIC/plugins/tools/ai.py
@@ -15,7 +15,7 @@ async def chat_gpt(bot, message):
 
         if len(message.command) < 2:
             await message.reply_text(
-                "✦ ᴇxᴀᴍᴘʟᴇ :-\n\n✦ /ask Where is TajMahal?"
+                "❖ ᴇxᴀᴍᴘʟᴇ ➥ /ask Where is TajMahal ?"
             )
         else:
             a = message.text.split(' ', 1)[1]
@@ -28,7 +28,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                         f"♥︎ {x}  \n\n✦ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➠  ๛ɴ ʏ ᴋ ᴀ ᴀ࿐",
+                         f"♥︎ {x}  \n\n❖ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➥  ๛ɴ ʏ ᴋ ᴀ ᴀ ࿐",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
