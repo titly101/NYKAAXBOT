339d7110a8be757e66e0649ee688d4fbae923598
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-15 00:37:13 +0530

    Update state.py

--- DAXXMUSIC/plugins/misc/state.py
+++ DAXXMUSIC/plugins/misc/state.py
@@ -1,6 +1,14 @@
 from pyrogram import Client, filters
 import pycountry
 from DAXXMUSIC import app 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+EVAA = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/nykaaxbot?startgroup=true"),
+    ],
+]
 
 @app.on_message(filters.command("gst"))
 def get_states(client, message):
@@ -13,14 +21,12 @@ def get_states(client, message):
         states = pycountry.subdivisions.get(country_code=country.alpha_2)
         states_list = [state.name for state in states]
         # Format the states into a message
-        states_message = f"๏ sᴛᴀᴛᴇs ᴏғ {country_name}\n" + "\n".join(states_list)
+        states_message = f"✦ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴄᴏᴜɴᴛʀʏ {country_name} ᴀʟʟ sᴛᴀᴛᴇs.\n\n" + " ❖ ".join(states_list) 
     except IndexError:
         # No country name was provided
-        states_message = "Please provide a country name after the command, like this:\n/get_states Canada"
+        states_message = "✦ ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴄᴏᴜɴᴛʀʏ ɴᴀᴍᴇ ᴀғᴛᴇʀ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ, ʟɪᴋᴇ ᴛʜɪs ➥ /cstats Japan"
     except AttributeError:
         # The country was not found
-        states_message = f"I couldn't find the country '{country_name}'. Please make sure it's spelled correctly."
+        states_message = f"✦ ɪ ᴄᴏᴜʟᴅɴ'ᴛ ғɪɴᴅ ᴛʜᴇ ᴄᴏᴜɴᴛʀʏ '{country_name}'. ᴘʟᴇᴀsᴇ ᴍᴀᴋᴇ sure ɪᴛ's sᴘᴇʟʟᴇᴅ ᴄᴏʀʀᴇᴄᴛʟʏ."
     
-    # Send the message with states
-    message.reply_text(states_message)
-    
+    message.reply_text(states_message, reply_markup=InlineKeyboardMarkup(EVAA),)
