d6d9d0b6b03c06e67bbc247baf535d25f7477257
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-15 01:06:53 +0530

    Update meme.py

--- DAXXMUSIC/plugins/Yumi/meme.py
+++ DAXXMUSIC/plugins/Yumi/meme.py
@@ -25,7 +25,7 @@ def meme_command(client, message):
         title = data.get("title")
 
         # Mention the bot username in the caption
-        caption = f"✦ {title}\n\n❅ ʀᴇǫᴜᴇsᴛ ʙʏ ➠ {message.from_user.mention}\n❅ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ [๛ɴ ʏ ᴋ ᴀ ᴀ࿐](https://t.me/h_cc_help)"
+        caption = f"❖ {title}\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [๛ɴ ʏ ᴋ ᴀ ᴀ ࿐](https://t.me/h_cc_help)"
 
         # Send the meme image to the user with the modified caption
         message.reply_photo(
