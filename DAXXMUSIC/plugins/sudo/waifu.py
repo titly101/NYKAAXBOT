335b4a21460a10ecab4a05d32a63cfb904c4d249
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-15 01:46:52 +0530

    Update waifu.py

--- DAXXMUSIC/plugins/sudo/waifu.py
+++ DAXXMUSIC/plugins/sudo/waifu.py
@@ -37,7 +37,7 @@ def waifu_command(client, message):
         if waifu_data and 'images' in waifu_data:
             first_image = waifu_data['images'][0]
             image_url = first_image['url']
-            message.reply_photo(image_url), caption=f"❖ ᴡᴀɪғᴜ ɪᴍɢ ʙʏ ➥ ๛ɴ ʏ ᴋ ᴀ ᴀ ࿐", reply_markup=InlineKeyboardMarkup(EVAA),)
+            message.reply_photo(image_url, caption=f"❖ ᴡᴀɪғᴜ ɪᴍɢ ʙʏ ➥ ๛ɴ ʏ ᴋ ᴀ ᴀ ࿐", reply_markup=InlineKeyboardMarkup(EVAA),)
         else:
             message.reply_text("No waifu found with the specified tags.")
 
