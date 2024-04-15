4af3cb9812046ac23fc7b0d9c6b6255531cc9834
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-15 17:51:25 +0530

    Update repo.py

--- DAXXMUSIC/plugins/bot/repo.py
+++ DAXXMUSIC/plugins/bot/repo.py
@@ -19,7 +19,7 @@ async def start(_, msg):
     buttons = [
         [
           InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/the_friendz"),
-          InlineKeyboardButton("ʀᴇᴘᴏ", callback_data="gib_source")
+          InlineKeyboardButton("ʀᴇᴘᴏ", utl="https://github.com/LOCO-PILOT/ROYMUSIC")
           ],
     ]
     
