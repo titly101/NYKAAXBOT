d0c66e21842f573ebe914d359ab12ea01df38f20
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-18 21:06:55 +0530

    Update Welcome.py

--- DAXXMUSIC/plugins/tools/Welcome.py
+++ DAXXMUSIC/plugins/tools/Welcome.py
@@ -127,7 +127,6 @@ async def greet_group(_, member: ChatMemberUpdated):
 ● ɴᴀᴍᴇ ➥  {user.mention}
 ● ᴜsᴇʀɴᴀᴍᴇ ➥  @{user.username}
 ● ᴜsᴇʀ ɪᴅ ➥  {user.id}
-
 ❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [๛ɴ ʏ ᴋ ᴀ ᴀ ࿐](https://t.me/nykaaxbot)**
 
 ▰▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▰
