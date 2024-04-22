4af47acc5f9dd7d78653b112de895a50ebdbacb9
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-04-22 18:55:38 +0530

    Update animelogo.py

--- DAXXMUSIC/plugins/tools/animelogo.py
+++ DAXXMUSIC/plugins/tools/animelogo.py
@@ -7,6 +7,9 @@ import requests
 from DAXXMUSIC import app
 from pyrogram.types import *
 
+
+#######
+
 button = [
        [
             InlineKeyboardButton(
@@ -14,6 +17,20 @@ button = [
             )
         ]
 ]
+
+#####
+
+@app.on_message(filters.command("logo"))
+async def logo(app, msg: Message):
+    if len(msg.command) == 1:
+       return await msg.reply_text("❖ ᴜsᴀɢᴇ ➥ /logo Nykaa")
+    logo_name = msg.text.split(" ", 1)[1]
+    API = f"https://api.sdbots.tech/logohq?text={logo_name}"
+    req = requests.get(API).url
+    await msg.reply_photo(
+        photo=f"{req}", caption=f"❖ ᴀɪ ʟᴏɢᴏ ɢᴇɴ ʙʏ ➥ [๛ɴ ʏ ᴋ ᴀ ᴀ ࿐](https://t.me/the_friendz)", reply_markup=InlineKeyboardMarkup(button),)
+
+######
 
 @app.on_message(filters.command("animelogo"))
 async def logo(app, msg: Message):
